3982a8baee04724f46f8d58c7adb9c5204efa892
Augusto <augusto.silva.contini@hotmail.com>
2024-08-08 18:08:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 function isPalindrome(str){
-    /* Seu código aqui */
-}
+    let strTratada = str.toLowerCase();
+    strTratada = strTratada.split(" ").join("");
+    let strInvertida = "";
+  
+    for (let i = 1; i <= strTratada.length; i++) {
+        strInvertida += strTratada[strTratada.length - i];
+    }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+    if (strTratada.toLowerCase() == strInvertida){
+    return true;
+    } else {
+    return false;
+    }
+  }
+  
+
+  function arrayMaxMin(arr){
+    let resultado = [arr[0], arr[0]]
+    for(let i = 0; i < arr.length; i++){
+      if (arr[i] > arr[i-1]){
+        if(arr[i] > resultado[1]){
+        resultado.pop();
+        resultado.push(arr[i]);
+        }
+      } else {
+        if(arr[i] < resultado[0]) {
+        resultado.shift();
+        resultado.unshift(arr[i]);
+        }
+      }
+    }
+    return resultado;
+  }
