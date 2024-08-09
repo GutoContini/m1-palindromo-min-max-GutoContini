38a927c9f62ab2ff64d4b1c6ea2b52e3ebf43183
Augusto <augusto.silva.contini@hotmail.com>
2024-08-09 09:33:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-function isPalindrome(str){
-    let strTratada = str.toLowerCase();
-    strTratada = strTratada.split(" ").join("");
-    let strInvertida = "";
-  
-    for (let i = 1; i <= strTratada.length; i++) {
-        strInvertida += strTratada[strTratada.length - i];
-    }
+function isPalindrome(str) {
+  let strTratada = str.toLowerCase();
+  strTratada = strTratada.split(" ").join("");
+  let strInvertida = "";
 
-    if (strTratada.toLowerCase() == strInvertida){
-    return true;
-    } else {
-    return false;
-    }
+  for (let i = 1; i <= strTratada.length; i++) {
+    strInvertida += strTratada[strTratada.length - i];
   }
-  
 
-  function arrayMaxMin(arr){
-    let resultado = [arr[0], arr[0]]
-    for(let i = 0; i < arr.length; i++){
-      if (arr[i] > arr[i-1]){
-        if(arr[i] > resultado[1]){
+  if (strTratada == strInvertida) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function arrayMaxMin(arr) {
+  let resultado = [arr[0], arr[0]];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) {
+      if (arr[i] > resultado[1]) {
         resultado.pop();
         resultado.push(arr[i]);
-        }
-      } else {
-        if(arr[i] < resultado[0]) {
+      }
+    } else {
+      if (arr[i] < resultado[0]) {
         resultado.shift();
         resultado.unshift(arr[i]);
-        }
       }
     }
-    return resultado;
   }
+  return resultado;
+}
